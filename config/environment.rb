d7a2012bf5f84d5eb8f729cbff01916e8c1b5ead
mccray austin <tubach75@hotmail.com>
2020-11-25 14:24:51 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -264,7 +264,7 @@ class MusicLibraryController
       #input = gets.strip.to_1
       song = list_songs[input -1]
       puts "Playing #{song.name} by #{song.artist.name}"
-      binding.pry
+      #binding.pry
     end
   end
 
