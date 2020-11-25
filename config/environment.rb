716d5bb135fd0fcdc286fffb81adaa868a3d8518
mccray austin <tubach75@hotmail.com>
2020-11-25 12:53:31 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -272,8 +272,7 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     input = gets.strip
     if artist = Artist.find_by_name(input) 
-    mary = artist.songs.sort{ |a, b| a.name <=> b.name }
-    mary.each.with_index(1) do |s, i| 
+      artist.songs.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i| 
       puts "#{i}. #{s.name} - #{s.genre.name}" 
       end 
     end
